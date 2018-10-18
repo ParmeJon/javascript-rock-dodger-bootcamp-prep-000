4a51f931fdd01d0bc649a0ff008ce00975e83ebc
Jonathan Chan <jonchan816@gmail.com>
2018-10-18 12:49:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -124,14 +124,11 @@ function createRock(x) {
  */
 function endGame() {
   clearInterval(gameInterval);
-  ROCKS.remove();
-  moveDodger.remove();
-  alert("YOU LOSE!");
-  
   for(var i = 0; i < ROCKS.length; i++) {
     ROCKS[i].remove();
   }
   window.removeEventListener('keydown', moveDodger);
+  alert("YOU LOSE!");
 }
 
 function moveDodger(e) {
